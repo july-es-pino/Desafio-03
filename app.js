9af4fca32a180bf66c267f0fa5f4b8e1ad16f5f6
july-es-pino <julia.espinosa.garcia@gmail.com>
2025-01-23 12:59:40 -0600

    Desafio--03

---
+++ app.js
@@ -0,0 +1,47 @@
+// Ejercicio 1:Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+
+//let numero = 1;
+ 
+/*while (numero <= 10){
+    alert (numero)
+    numero = numero + 1
+};*/
+
+// Ejercicio 2: Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+
+
+/*let numero = 10;
+
+while (numero >= 0){
+   alert (numero)
+
+   numero = numero - 1
+};*/
+
+/*Ejercio: 3 Crea un programa de cuenta regresiva. 
+Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.*/
+
+/*let numeroIngresado = prompt ("Ingresa un numero del 1 al 20, para la cuenta regresiva")
+
+while(numeroIngresado >= 0){
+
+    console.log (numeroIngresado);
+
+    numeroIngresado --;
+
+};*/
+
+/* Ejercicio 4:Crea un programa de cuenta progresiva. 
+Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.*/
+
+
+/*let numeroUsuario = prompt ("Ingresa un numero menor a 100 para la cuenta progresiva ");
+let contador = 0;
+
+while (contador <= numeroUsuario){
+
+console.log (contador);
+contador ++
+
+};*/
+
